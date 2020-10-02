784c7f0f7dcba4fe4d75b8a0f1b4ebb9b322cbf9
shubham 7822 <shubhammansute7822@gmail.com>
2020-10-02 08:01:45 +0530

    Updates

---
+++ precache-manifest.8b7636452c82dbc41514aedce2d5f72e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb67a5f0234c830dd77968d03ba42a7",
+    "url": "/messanger-clone/index.html"
+  },
+  {
+    "revision": "1ea4eba090e86e87a43e",
+    "url": "/messanger-clone/static/css/main.0b553000.chunk.css"
+  },
+  {
+    "revision": "4553962e977239d7df3e",
+    "url": "/messanger-clone/static/js/2.65432bbd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messanger-clone/static/js/2.65432bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea4eba090e86e87a43e",
+    "url": "/messanger-clone/static/js/main.518cd584.chunk.js"
+  },
+  {
+    "revision": "678813b61d4f2bec56c3",
+    "url": "/messanger-clone/static/js/runtime-main.cd30e887.js"
+  }
+]);
